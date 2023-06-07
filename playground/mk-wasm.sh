c03c2a2fd843c9c546bc5d633ddcd0b2a653d6a2
mingodad <mingodad@gmail.com>
2023-06-07 18:58:38 +0200

    Update to Lua-5.4.6

--- playground/mk-wasm.sh
+++ playground/mk-wasm.sh
@@ -6,7 +6,7 @@ emsdk-env emcc  \
 	-Os -DMAKE_LUA_WASM -DLUA_PROGNAME='"lua"' \
 	-DLUA_COMPAT_5_3 -DLUA_USE_LINUX -D_XOPEN_SOURCE=500 \
 	-DWITH_LPEGLABEL -DWITH_LUAFILESYSTEM -DMAKE_LUA_CMD -DMAKE_LUAC_CMD \
-	-o teal-lua-playground.js am-lua-5.4.4.c \
+	-o teal-lua-playground.js am-lua-5.4.6.c \
 	-sEXPORTED_FUNCTIONS=_main,_lua_main,_luac_main,_free,_malloc \
 	-sEXPORTED_RUNTIME_METHODS=ccall,cwrap,FS,callMain,setValue \
 	-sALLOW_MEMORY_GROWTH -s INVOKE_RUN=0 -s EXIT_RUNTIME=0 \
